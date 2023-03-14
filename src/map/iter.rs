1cf9b1ce8dd19f7a3e49c967bbb5709a70cb4cf5
ultrabear <bearodark@gmail.com>
2023-03-14 14:42:07 -0700

    add Iter and IterMut to Map private impls

--- src/map/iter.rs
+++ src/map/iter.rs
@@ -1,6 +1,8 @@
 extern crate alloc;
 
-/// Iterator from a [`PrefixArray`][super::PrefixArray]
+use core::iter::FusedIterator;
+
+/// Consuming Iterator from a [`PrefixArray`][super::PrefixArray]
 pub struct IntoIter<K: AsRef<str>, V>(alloc::vec::IntoIter<(K, V)>);
 
 impl<K: AsRef<str>, V> Iterator for IntoIter<K, V> {
@@ -15,12 +17,60 @@ impl<K: AsRef<str>, V> Iterator for IntoIter<K, V> {
     }
 }
 
-impl<K: AsRef<str>, V> core::iter::FusedIterator for IntoIter<K, V> {}
+impl<K: AsRef<str>, V> FusedIterator for IntoIter<K, V> {}
 impl<K: AsRef<str>, V> ExactSizeIterator for IntoIter<K, V> {}
 
 impl<K: AsRef<str>, V> DoubleEndedIterator for IntoIter<K, V> {
     fn next_back(&mut self) -> Option<Self::Item> {
         self.0.next_back()
+    }
+}
+
+/// Immutable view Iterator from a [`PrefixArray`][super::PrefixArray] or [`SubSlice`][super::SubSlice]
+pub struct Iter<'a, K: AsRef<str>, V>(core::slice::Iter<'a, (K, V)>);
+
+impl<'a, K: AsRef<str>, V> Iterator for Iter<'a, K, V> {
+    type Item = (&'a K, &'a V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|(k, v)| (k, v))
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+impl<'a, K: AsRef<str>, V> FusedIterator for Iter<'a, K, V> {}
+impl<'a, K: AsRef<str>, V> ExactSizeIterator for Iter<'a, K, V> {}
+
+impl<'a, K: AsRef<str>, V> DoubleEndedIterator for Iter<'a, K, V> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.0.next_back().map(|(k, v)| (k, v))
+    }
+}
+
+/// Mutable view Iterator from a [`PrefixArray`][super::PrefixArray] or [`SubSlice`][super::SubSlice]
+pub struct IterMut<'a, K: AsRef<str>, V>(core::slice::IterMut<'a, (K, V)>);
+
+impl<'a, K: AsRef<str>, V> Iterator for IterMut<'a, K, V> {
+    type Item = (&'a K, &'a mut V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|(k, v)| (&*k, v))
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+impl<'a, K: AsRef<str>, V> FusedIterator for IterMut<'a, K, V> {}
+impl<'a, K: AsRef<str>, V> ExactSizeIterator for IterMut<'a, K, V> {}
+
+impl<'a, K: AsRef<str>, V> DoubleEndedIterator for IterMut<'a, K, V> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.0.next_back().map(|(k, v)| (&*k, v))
     }
 }
 
