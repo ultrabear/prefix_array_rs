0082286c8c037dc288882316ff994975eda8952e
ultrabear <bearodark@gmail.com>
2023-03-14 14:53:33 -0700

    copy impl of Iter to Set api

--- src/set/iter.rs
+++ src/set/iter.rs
@@ -1,5 +1,7 @@
 extern crate alloc;
 
+use core::iter::FusedIterator;
+
 /// Iterator from a [`PrefixArraySet`][super::PrefixArraySet].
 pub struct IntoIter<K: AsRef<str>>(crate::map::IntoIter<K, ()>);
 
@@ -15,10 +17,34 @@ impl<K: AsRef<str>> Iterator for IntoIter<K> {
     }
 }
 
-impl<K: AsRef<str>> core::iter::FusedIterator for IntoIter<K> {}
+impl<K: AsRef<str>> FusedIterator for IntoIter<K> {}
 impl<K: AsRef<str>> ExactSizeIterator for IntoIter<K> {}
 
 impl<K: AsRef<str>> DoubleEndedIterator for IntoIter<K> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.0.next_back().map(|(k, _)| k)
+    }
+}
+
+/// Immutable view Iterator from a [`PrefixArraySet`][super::PrefixArraySet] or [`SetSubSlice`][super::SetSubSlice]
+pub struct Iter<'a, K: AsRef<str>>(pub(super) crate::map::Iter<'a, K, ()>);
+
+impl<'a, K: AsRef<str>> Iterator for Iter<'a, K> {
+    type Item = &'a K;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|(k, _)| k)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+impl<'a, K: AsRef<str>> FusedIterator for Iter<'a, K> {}
+impl<'a, K: AsRef<str>> ExactSizeIterator for Iter<'a, K> {}
+
+impl<'a, K: AsRef<str>> DoubleEndedIterator for Iter<'a, K> {
     fn next_back(&mut self) -> Option<Self::Item> {
         self.0.next_back().map(|(k, _)| k)
     }
